2b673349f6886f91efb6d1ff8128cda9122fda09
Mikael Lind <elemel@elemel.se>
2008-11-04 23:12:23 +0100

    Implemented the Monster class.

---
+++ src/monsters.lua
@@ -0,0 +1,12 @@
+local Monster = {}
+
+function Monster:new(monster)
+    monster = monster or {}
+    setmetatable(monster, self)
+    self.__index = self
+    return monster
+end
+
+local monsters = {Monster = Monster}
+
+return monsters
